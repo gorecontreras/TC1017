b9aafda8aa549936bc87bb4db78ac0142c988e12
gorecontreras <goretty.contrerasolivo@gmail.com>
2015-09-16 22:59:48 -0500

    Update sum.cpp

--- sum.cpp
+++ sum.cpp
@@ -2,15 +2,14 @@
 #include <cstdlib>
 using namespace std;
 int main(){
-  int a, b, c, s=0;
+  int a, b, s=0;
   cout<<"De el rango"<<endl;
   cout<<"Primer numero:"<<endl;
   cin>>a;
   cout<<"Segundo numero"<<endl;
   cin>>b;
-  c=b-a+1;
-  while (a<=b)
-  {s=s+a;
+  while (a<=b){
+    s=s+a;
   a++;}
   cout<<"La suma es:"<<s<<endl;
 }
